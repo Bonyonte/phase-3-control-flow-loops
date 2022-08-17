3356f7f6683bcb72ea7b1c03469f4a15656e8924
Bonyonte <godfrey.magogo@student.moringaschool.comgit config --global user.name Bonyontegit branch -m maingit branch -M maingit remote add origin git@github.com:Bonyonte/ip_1.gitgit push -u origin maingit config --global user.email godfrey.magogo@student.moringaschool.com>
2022-08-17 20:07:31 +0300

    commit tree b5824ed22af0e5971ce358dc981127293d31b733

--- looping.rb
+++ looping.rb
@@ -1,7 +1,12 @@
 def happy_new_year
   # your code here
+  counter = 11
+  while counter > 1
+    puts counter -=1
+  end
+    puts"Happy New Year!"
 end
-
+puts happy_new_year
 # No need to modify this code! Use this to implement the fizzbuzz_printer method.
 def fizzbuzz(num)
   if num % 3 == 0 && num % 5 == 0
@@ -17,8 +22,26 @@ end
 
 def fizzbuzz_printer
   # your code here
+  (1..100).each do |num|
+    if num % 3 == 0 && num % 5 == 0
+      puts"FizzBuzz"
+    elsif num % 3 == 0
+      puts"Fizz"
+    elsif num % 5 == 0
+      puts"Buzz"
+    else
+      puts num
+    end
+  end
 end
 
 def reverse_string(str)
   # your code here
+  reversed_string = ""
+  string_length = str.length - 1
+  string_length.downto(0).each do |i|
+    reversed_string << str [i]
 end
+reversed_string
+end
+puts reverse_string("hello")
